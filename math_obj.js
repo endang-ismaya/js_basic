4c5c89d48fd41ff67044999b568ccd4e2e45da22
endang-ismaya <endang.ismaya@gmail.com>
2025-10-16 21:24:44 +0700

    add Math Object

---
+++ math_obj.js
@@ -0,0 +1,16 @@
+let x;
+
+x = Math.sqrt(100); // 10 number
+x = Math.abs(-5); // 5 number
+x = Math.pow(8, 2); // 64 number
+x = Math.round(4.2); // 4 number
+x = Math.ceil(4.2); // 5 number
+x = Math.floor(4.2); // 4 number
+x = Math.min(1, 2, 3, 4, 5); // 1 number
+x = Math.max(1, 2, 3, 4, 5); // 5 number
+x = Math.random(); // 0.
+
+x = Math.floor(Math.random() * 10 + 1); // 1-10 number
+
+// print to console
+console.log(x, typeof x);
